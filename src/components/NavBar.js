b16707e6b8485c9478f23c726e75e87e3a306090
shalom-nganga <minnomacharia@gmail.com>
2023-11-15 19:52:43 +0300

    completed assignments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index}>
+            <a href={`#${link}`}>{link}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
